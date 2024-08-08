c8618d038cd4cbad47df55ac82bd10d9f60b17f7
Spencer Perkins <95900100+SpecialThing44@users.noreply.github.com>
2024-08-07 23:30:13 -0400

    Change get_min_sequence_diff to use AMINO_ACIDS constant

--- src/plcg/sequence_scoring/sequence_difference.py
+++ src/plcg/sequence_scoring/sequence_difference.py
@@ -10,7 +10,7 @@ def get_sequence_diff(sequence_one: list[int], sequence_two: str) -> float:
     return sequence_diff
 
 
-def get_min_sequence_diff(sequence, s1: list[list[int]], s2: str) -> float:
-    s1 = [num_list_to_aa_seq(sequence, x) for x in s1]
+def get_min_sequence_diff(s1: list[list[int]], s2: str) -> float:
+    s1 = [num_list_to_aa_seq(AMINO_ACIDS, x) for x in s1]
     sequence_diff = [Levenshtein.distance(x, s2) for x in s1]
     return min(sequence_diff)
