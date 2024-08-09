96d2cc37e011578381c36f76aa991a7ca34eb323
Spencer Perkins <95900100+SpecialThing44@users.noreply.github.com>
2024-08-08 22:45:34 -0400

    Fix import paths and sequence diff utils (#10)

--- src/plcg/sequence_scoring/sequence_difference.py
+++ src/plcg/sequence_scoring/sequence_difference.py
@@ -1,16 +1,15 @@
 import Levenshtein
 
-from src.plcg.sequence_scoring.sequence_difference import num_list_to_aa_seq
-from src.plcg.constants.values import AMINO_ACIDS
+from plcg.sequence_scoring.sequence_to_number_conversions import num_list_to_aa_seq
 
 
 def get_sequence_diff(sequence_one: list[int], sequence_two: str) -> float:
-    sequence_one = num_list_to_aa_seq(AMINO_ACIDS, sequence_one)
+    sequence_one = num_list_to_aa_seq(sequence_one)
     sequence_diff = Levenshtein.distance(sequence_one, sequence_two)
     return sequence_diff
 
 
-def get_min_sequence_diff(sequence, s1: list[list[int]], s2: str) -> float:
-    s1 = [num_list_to_aa_seq(sequence, x) for x in s1]
+def get_min_sequence_diff(s1: list[list[int]], s2: str) -> float:
+    s1 = [num_list_to_aa_seq(x) for x in s1]
     sequence_diff = [Levenshtein.distance(x, s2) for x in s1]
     return min(sequence_diff)
