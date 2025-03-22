d41e87fbbe3570a6d55cfc109363301752931b7f
Spencer Perkins <95900100+SpecialThing44@users.noreply.github.com>
2025-03-22 16:32:13 -0400

    Return ptm from batch colabfold

--- src/plcg/structure_prediction/colabfold/colabfold.py
+++ src/plcg/structure_prediction/colabfold/colabfold.py
@@ -2,6 +2,7 @@ import csv
 import os
 import shutil
 import subprocess
+import json
 
 from plcg.structure_prediction.fasta.fasta import save_fasta_file
 
@@ -48,7 +49,7 @@ def run_colabfold_and_get_filepath(seq: str, working_dir: str):
     if len(json_filepath) > 0:
         json_filepath = json_filepath[0]
         with open(working_dir + json_filepath, "r", encoding="utf-8") as f:
-            data = f.read()
+            data = json.load(f)
             ptm_value = data.get("ptm")
 
     return pdb_filepath, ptm_value
@@ -76,11 +77,15 @@ def run_colabfold_batch_and_return_filepaths(sequences: list[str], working_dir: 
     save_fasta_file(sequences, working_dir + fasta_file_path)
     colabfold_batch(working_dir + fasta_file_path, working_dir)
     sequences_to_pdb_filepaths = {}
+    sequences_to_json_filepaths = {}
     files = os.listdir(working_dir)
     for i, sequence in enumerate(sequences):
         results = [result for result in files if "Sequence_" + str(i) in result]
         result = [result for result in results if "unrelaxed_rank_001" in result]
+        json_filepath = [result for result in results if "scores_rank_001" in result]
         if len(result) > 0:
             result = result[0]
+            json_filepath = json_filepath[0]
             sequences_to_pdb_filepaths[i] = working_dir + result
-    return sequences_to_pdb_filepaths
+            sequences_to_json_filepaths[i] = working_dir + json_filepath
+    return sequences_to_pdb_filepaths, sequences_to_json_filepaths
