576272943c3e30bce9b2925f69759ab742ab6897
Spencer Perkins <95900100+SpecialThing44@users.noreply.github.com>
2025-03-22 16:36:49 -0400

    Return ptm from batch colabfold (#15)
    * Return ptm from batch colabfold
    
    * Use the version as the release name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="plcg",
-    version="1.0.1",
+    version="1.0.3",
     author="Spencer Perkins",
     author_email="spencer.perkins44sp@gmail.com",
     description="Computational biology and machine learning utilities for the Pardee Lab Computation Group",
